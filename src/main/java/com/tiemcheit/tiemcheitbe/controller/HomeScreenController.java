dbc643fc3c44c61596de0e99570d252fe436915c
Nguyễn Kim Long <longnguyenkim2707@gmail.com>
2024-05-23 13:32:38 +0700

    refector: change GetMapping from /home and category?id= to /home/category and allProductByCategory/{id}

--- src/main/java/com/tiemcheit/tiemcheitbe/controller/HomeScreenController.java
+++ src/main/java/com/tiemcheit/tiemcheitbe/controller/HomeScreenController.java
@@ -6,8 +6,8 @@ import com.tiemcheit.tiemcheitbe.service.CategoryService;
 import com.tiemcheit.tiemcheitbe.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,15 +20,15 @@ public class HomeScreenController {
     private final CategoryService categoryService;
     private final ProductService productService;
 
-    // this request is:  http://localhost:8080/api/home/categories
-    @GetMapping("")
+    // this request is:  http://localhost:8080/api/home/category
+    @GetMapping("/category")
     public List<CategoryDTO> getAllCategories() {
         return categoryService.getAllCategories();
     }
 
-    // this request is:  http://localhost:8080/api/home/category?id=
-    @GetMapping("/category")
-    public List<ProductDTO> getAllProductsByCategoryID(@RequestParam Long id) {
+    // this request is:  http://localhost:8080/api/home/allProductByCategory/{id}
+    @GetMapping("/allProductByCategory/{id}")
+    public List<ProductDTO> getAllProductsByCategoryID(@PathVariable Long id) {
         return productService.getAllProductsByCategoryId(id);
     }
 }
