d29c5095eeff7501d2c1c1b20c7cb492f9940387
Dung Nguyen Viet <realvietdung@gmail.com>
2024-05-27 23:27:57 +0700

    refactor: remove merge error

--- src/main/java/com/tiemcheit/tiemcheitbe/repository/UserAddressRepo.java
+++ src/main/java/com/tiemcheit/tiemcheitbe/repository/UserAddressRepo.java
@@ -1,7 +1,7 @@
-//package com.tiemcheit.tiemcheitbe.repository;
-//
-//import com.tiemcheit.tiemcheitbe.model.UserAddress;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserAddressRepo extends JpaRepository<UserAddress, Long> {
-//}
+package com.tiemcheit.tiemcheitbe.repository;
+
+import com.tiemcheit.tiemcheitbe.model.UserAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAddressRepo extends JpaRepository<UserAddress, Long> {
+}
