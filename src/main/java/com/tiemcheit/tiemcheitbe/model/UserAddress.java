3df6c4cb217745d0abef85cbcaba4364bb89eec3
DucNguyen2004 <manhduc112004@gmail.com>
2024-06-05 14:00:12 +0700

    fix: fix attribute of user address

--- src/main/java/com/tiemcheit/tiemcheitbe/model/UserAddress.java
+++ src/main/java/com/tiemcheit/tiemcheitbe/model/UserAddress.java
@@ -17,7 +17,7 @@ public class UserAddress {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "username", referencedColumnName = "username", nullable = false)
     private User user;
 
     @Column(nullable = false, length = 256)
