ac993270d2f2caf66a2402313d2a7e8ab85fc511
nkl04 <longnguyenkim2707@gmail.com>
2024-06-10 22:39:22 +0700

    refactor: change username to user_id in useraddress

--- src/main/java/com/tiemcheit/tiemcheitbe/model/UserAddress.java
+++ src/main/java/com/tiemcheit/tiemcheitbe/model/UserAddress.java
@@ -17,7 +17,7 @@ public class UserAddress {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "username", referencedColumnName = "username", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Column(nullable = false, length = 256)
