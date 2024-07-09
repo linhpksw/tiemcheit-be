96ffb7e818b0a7d42beab21c5a2c84f427307598
thangnt <tatthang283@gmail.com>
2024-07-09 13:47:32 +0700

    feat: customer manage

---
+++ src/main/java/com/tiemcheit/tiemcheitbe/controller/EmployeeController.java
@@ -0,0 +1,55 @@
+package com.tiemcheit.tiemcheitbe.controller;
+
+import com.tiemcheit.tiemcheitbe.dto.request.EmployeeRequest;
+import com.tiemcheit.tiemcheitbe.dto.response.ApiResponse;
+import com.tiemcheit.tiemcheitbe.dto.response.UserProfileResponse;
+import com.tiemcheit.tiemcheitbe.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/employees")
+public class EmployeeController {
+
+    public final EmployeeService employeeService;
+
+    @GetMapping("")
+    public ApiResponse<List<UserProfileResponse>> allCustomers() {
+        var data = employeeService.allEmployees();
+        return ApiResponse.<List<UserProfileResponse>>builder()
+                .message("Success")
+                .data(data).build();
+    }
+
+    @GetMapping("/{uid}")
+    public ApiResponse<UserProfileResponse> getCustomerInfo(@PathVariable Long uid) {
+        var data = employeeService.getEmployeeById(uid);
+        return ApiResponse.<UserProfileResponse>builder()
+                .message("Success")
+                .data(data).build();
+    }
+
+    @PatchMapping("")
+    public ApiResponse<UserProfileResponse> editCustomerInfo(@RequestBody EmployeeRequest employeeRequest) {
+        var data = employeeService.updateEmployee(employeeRequest);
+        return ApiResponse.<UserProfileResponse>builder()
+                .message("Success")
+                .data(data).build();
+    }
+
+//    @GetMapping("/filter")
+//    public ApiResponse<List<UserProfileResponse>> filteredCustomers(
+//            @RequestParam(required = false) String status,
+//            @RequestParam(required = false) String sortOption,
+//            @RequestParam(required = false) String order) {
+//        var data = customerService.getCustomersByStatus(status, sortOption, order);
+//        return ApiResponse.<List<UserProfileResponse>>builder()
+//                .message("Success")
+//                .data(data)
+//                .build();
+//    }
+
+}
---
+++ src/main/java/com/tiemcheit/tiemcheitbe/service/EmployeeService.java
@@ -0,0 +1,104 @@
+package com.tiemcheit.tiemcheitbe.service;
+
+import com.tiemcheit.tiemcheitbe.dto.request.EmployeeRequest;
+import com.tiemcheit.tiemcheitbe.dto.response.UserProfileResponse;
+import com.tiemcheit.tiemcheitbe.mapper.RoleMapper;
+import com.tiemcheit.tiemcheitbe.mapper.UserMapper;
+import com.tiemcheit.tiemcheitbe.model.Role;
+import com.tiemcheit.tiemcheitbe.repository.OrderRepo;
+import com.tiemcheit.tiemcheitbe.repository.RoleRepo;
+import com.tiemcheit.tiemcheitbe.repository.UserRepo;
+import com.tiemcheit.tiemcheitbe.repository.exception.AppException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+    private final OrderRepo orderRepo;
+    private final UserMapper userMapper;
+    private final UserService userService;
+    private final RoleMapper roleMapper;
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public List<UserProfileResponse> allEmployees() {
+        List<UserProfileResponse> users = userService.getUsersProfile();
+        List<UserProfileResponse> employees = new ArrayList<>();
+        Role role = roleRepo.findByName("EMPLOYEE").orElseThrow(() -> new AppException("Role not found.", HttpStatus.NOT_FOUND));
+        for (UserProfileResponse u : users) {
+            if (u.getRoles().contains(roleMapper.toRoleResponse(role))) {
+                u.setOrderNumber(orderRepo.countByUser_Id(u.getId()));
+                u.setOrderTotal(orderRepo.getTotalAmountSpentByUser(u.getId()));
+                employees.add(u);
+            }
+        }
+
+        return employees;
+    }
+
+    public UserProfileResponse getEmployeeById(Long id) {
+        return userMapper.toUserProfileResponse(userRepo.getReferenceById(id));
+    }
+
+    public UserProfileResponse updateEmployee(EmployeeRequest employeeRequest) {
+        return userService.updateUserProfile(employeeRequest.getUsername(), employeeRequest.getUpdateData());
+    }
+
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//    public List<UserProfileResponse> getEmployeesByStatus(String status, String sortOption, String order) {
+//        if (status == null && sortOption == null && order == null) {
+//            return allEmployees();
+//        }
+//
+//        List<User> customers = new ArrayList<>();
+//        String uppercaseStatus = status != null ? status.toUpperCase() : null;
+//        if (sortOption != null) {
+//            switch (sortOption.toLowerCase()) {
+//                case "created_at":
+//                    if (order.equalsIgnoreCase("asc")) {
+//                        customers = userRepo.findAscCustomersOrderByCreatedAt(uppercaseStatus);
+//                    } else {
+//                        customers = userRepo.findDescCustomersOrderByCreatedAt(uppercaseStatus);
+//                    }
+//                    break;
+//                case "order_number":
+//                    if (order.equalsIgnoreCase("asc")) {
+//                        customers = userRepo.findAscCustomersOrderByOrderNumber(uppercaseStatus);
+//                    } else {
+//                        customers = userRepo.findDescCustomersOrderByOrderNumber(uppercaseStatus);
+//                    }
+//                    break;
+//                case "order_total":
+//                    if (order.equalsIgnoreCase("asc")) {
+//                        customers = userRepo.findAscCustomersOrderByOrderTotal(uppercaseStatus);
+//                    } else {
+//                        customers = userRepo.findDescCustomersOrderByOrderTotal(uppercaseStatus);
+//                    }
+//                    break;
+//            }
+//        } else {
+//            customers = userRepo.findCustomerByStatus(status);
+//        }
+//        List<UserProfileResponse> responseCustomers = new ArrayList<>();
+//        for (User c : customers) {
+//            responseCustomers.add(userMapper.toUserProfileResponse(c));
+//        }
+//
+//        Role role = roleRepo.findByName("CUSTOMER").orElseThrow(() -> new AppException("Role not found.", HttpStatus.NOT_FOUND));
+//        for (UserProfileResponse u : responseCustomers) {
+//            if (u.getRoles().contains(roleMapper.toRoleResponse(role))) {
+//                u.setOrderNumber(orderRepo.countByUser_Id(u.getId()));
+//                u.setOrderTotal(orderRepo.getTotalAmountSpentByUser(u.getId()));
+//            }
+//        }
+//        return responseCustomers;
+//    }
+}
