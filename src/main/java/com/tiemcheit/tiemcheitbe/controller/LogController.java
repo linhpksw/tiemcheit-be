76284ce88bab0ce93c694fd012f9e65857dcc814
Trọng Linh <linhpksw@gmail.com>
2024-07-15 14:35:12 +0700

    feat: add feature for logs

--- src/main/java/com/tiemcheit/tiemcheitbe/controller/LogController.java
+++ src/main/java/com/tiemcheit/tiemcheitbe/controller/LogController.java
@@ -6,18 +6,22 @@ import com.tiemcheit.tiemcheitbe.dto.response.LogResponse;
 import com.tiemcheit.tiemcheitbe.service.LogService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/logs")
 public class LogController {
     private final LogService logService;
@@ -26,14 +30,21 @@ public class LogController {
     public ApiResponse<Map<String, Object>> getAllLogs(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
-            @RequestParam(defaultValue = "desc") String sortDirection) {
+            @RequestParam(defaultValue = "desc") String sortDirection,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+            @RequestParam(defaultValue = "all") String status) {
 
-        Sort.Direction direction = sortDirection.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+//        log.info("Sort direction received: {}", sortDirection);
+
+        Sort.Direction direction = sortDirection.equalsIgnoreCase("asc")
+                ? Sort.Direction.ASC : Sort.Direction.DESC;
 
         // Sorting logs by updated time in descending order
         Pageable sortedByUpdatedDesc = PageRequest.of(page, size, Sort.by(direction, "timestamp"));
 
-        Page<LogResponse> logs = logService.findAllLogs(sortedByUpdatedDesc);
+        Page<LogResponse> logs = logService.findLogsByDateRange(sortedByUpdatedDesc, startDate, endDate, status);
+
         Map<String, Object> responseData = new HashMap<>();
         responseData.put("logs", logs.getContent());
         responseData.put("totalPages", logs.getTotalPages());
